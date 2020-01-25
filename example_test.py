e301b400db61d96464f9982fd12333cace5ac25c
amy727 <achen.com@gmail.com>
2020-01-25 12:03:08 -0500

    Updated example test to read data from image file 1

--- example_test.py
+++ example_test.py
@@ -6,4 +6,8 @@ def ocr_core(filename):
     text = pytesseract.image_to_string(Image.open(filename))
     return text
 
-print(ocr_core('example.png'))
+
+if __name__ == "__main__":
+
+    filename = "./flyer_images/week_1_page_1.jpg"
+    print(ocr_core(filename))
