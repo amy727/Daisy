cea61fa2195c964c51ce0b1e62b895b01cbee285
amy727 <achen.com@gmail.com>
2020-01-25 17:30:55 -0500

    Added more parsing capabilities

--- word_processor.py
+++ word_processor.py
@@ -13,14 +13,14 @@ def word_process(filename):
         for row in f:
             row = row.rstrip()
             product_names.append(row)
-    print(product_names)
+    #print(product_names)
 
     with open("units_dictionary.csv", "r") as f:
         units = []
         for row in f:
             row = row.rstrip()
             units.append(row)
-    print(units)
+    #print(units)
 
     #open file
     textfile = open(filename, "r")
@@ -32,13 +32,81 @@ def word_process(filename):
         line = line.rstrip()
         line = line.lstrip()
 
+        #if product name is only on one line
         if line in product_names:
             product["product_name"] = line
-    
 
+        #if line starts with SAVE
+        elif line.startswith("SAVE"):
+            #split line by $ sign
+            split_line = line.split("$")
+            #print(split_line)
+            amt_saved = ""
+            
+            #check if character is numeric
+            for ch in split_line[1]:
+                try:
+                    int(ch)
+                    amt_saved = amt_saved + ch
+                    print(amt_saved)
+                except:
+                    break #break from loop
+            
+            product["save_per_unit"] = amt_saved
+    
+        #if line starts with $
+        elif line.startswith("$"):
+            
+            line = line.lstrip("$")
+
+            #find unit
+            for unit in units:
+                if unit in line: #add unit to uom
+                    product["uom"] = unit
+
+            split_line = line.split("/")
+            
+            #Determine price
+            price = determine_price(split_line[0])
+            product["unit_promo_price"] = price
+
+        #if the line starts with a digit
+        elif len(line)>0 and line[0].isdigit():
+
+            split_line = line.split("/")
+            product["least_unit_for_promo"] = int(split_line[0])
+
+            split_line[1] = split_line[1].lstrip("$")
+            price = determine_price(split_line[1])
+            product["unit_promo_price"] = price
+
+            
+
+
+
+
+    #close file
     textfile.close()
 
     return product
+
+def determine_price(str_price):
+    """
+    (str) -> num
+    """
+
+    for ch in str_price:
+        if not ch.isdigit():
+            str_price = str_price.replace(ch, "")
+
+    if len(str_price) >= 3:
+        price = float(str_price)/100
+            
+        
+    else:
+        price = float(str_price)
+
+    return price
 
 if __name__ == "__main__":
     
